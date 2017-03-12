7b8e8534396094aa09c0cab3e880f402461c8811
knight42 <anonymousknight96@gmail.com>
2017-03-12 11:47:54 +0800

    Update documentation

--- api_project.js
+++ api_project.js
@@ -1,6 +1,6 @@
 define({
   "name": "ustcmirror",
-  "version": "0.1.4",
+  "version": "0.1.6",
   "description": "API documentation",
   "title": "ustcmirror API documentation",
   "url": "https://admin.mirrors.ustc.edu.cn/api/v1",
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-02-15T15:18:01.956Z",
+    "time": "2017-03-12T03:43:23.658Z",
     "url": "http://apidocjs.com",
     "version": "0.17.5"
   }
